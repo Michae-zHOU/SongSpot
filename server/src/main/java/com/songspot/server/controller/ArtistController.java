273ceba96d00583a5b78209bea3535da9a5254b2
Michael Zhou <michael.zhouzzy@gmail.com>
2020-07-26 18:04:10 -0700

    Change username fetch in controller

--- server/src/main/java/com/songspot/server/controller/ArtistController.java
+++ server/src/main/java/com/songspot/server/controller/ArtistController.java
@@ -1,8 +1,10 @@
 package com.songspot.server.controller;
 
+import com.songspot.server.client.UserClient;
 import com.songspot.server.controller.model.CreateDemoTrack;
 import com.songspot.server.controller.model.DemoTrack;
 import com.songspot.server.repository.DemoTrackDaoJpa;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +21,18 @@ public class ArtistController {
     @Autowired
     private DemoTrackDaoJpa trackDaoJpa;
 
+
     @PostMapping(ARTIST_CREATE_TRACK_ROUTE)
     @ResponseStatus(HttpStatus.CREATED)
     public DemoTrack createDemoTrack(@Valid @RequestBody CreateDemoTrack createDemoTrack) {
-        return trackDaoJpa.createDemoTrack(getRequesterUsername(), createDemoTrack);
+        String username = MDC.get(UserClient.USER_NAME_KEY);
+        return trackDaoJpa.createDemoTrack(username, createDemoTrack);
     }
 
     @GetMapping(ARTIST_VIEW_TRACKS_ROUTE)
     @ResponseStatus(HttpStatus.OK)
     public List<DemoTrack> getDemoTrack() {
-        return trackDaoJpa.getDemoTrackOfArtist(getRequesterUsername());
-    }
-
-    private String getRequesterUsername() {
-        return "default_user";
+        String username = MDC.get(UserClient.USER_NAME_KEY);
+        return trackDaoJpa.getDemoTrackOfArtist(username);
     }
 }
--- server/src/main/java/com/songspot/server/controller/CuratorController.java
+++ server/src/main/java/com/songspot/server/controller/CuratorController.java
@@ -1,25 +1,29 @@
 package com.songspot.server.controller;
 
-import com.songspot.server.controller.model.CreateDemoTrack;
+import com.songspot.server.client.UserClient;
 import com.songspot.server.controller.model.DemoTrack;
 import com.songspot.server.repository.DemoTrackDaoJpa;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class CuratorController {
 
-    protected static final String CURATOR_VIEW_ROUTE = "/curator/view/{curator}";
+    protected static final String CURATOR_VIEW_ROUTE = "/curator/view/";
 
     @Autowired
     private DemoTrackDaoJpa trackDaoJpa;
 
     @GetMapping(CURATOR_VIEW_ROUTE)
     @ResponseStatus(HttpStatus.CREATED)
-    public DemoTrack createDemoTrack(@Valid @RequestBody CreateDemoTrack createDemoTrack) {
-        return trackDaoJpa.createDemoTrack(null, createDemoTrack);
+    public List<DemoTrack> getTracks() {
+        String username = MDC.get(UserClient.USER_NAME_KEY);
+        return trackDaoJpa.getDemoTrackOfCurator(username);
     }
 }
