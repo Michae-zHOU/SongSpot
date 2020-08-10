a80bed713d055973dbbafa450df0c3aec353155d
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-10 11:56:20 -0700

    SpringRedis and Redis Config

---
+++ server/src/main/java/com/songspot/server/configuration/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.songspot.server.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "com.songspot.server.redis", enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+public class RedisConfig {
+
+    @Autowired
+    private SpringRedisConfig springRedisConfig;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(
+                new RedisStandaloneConfiguration(springRedisConfig.getHost(), springRedisConfig.getPort()));
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return template;
+    }
+}
