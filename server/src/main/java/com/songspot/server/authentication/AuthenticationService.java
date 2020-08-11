8454bfacd81f9669a609d0b98d6b8e153bb6f2d6
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-11 01:42:47 -0700

    Implemented AuthenticationService

--- server/src/main/java/com/songspot/server/authentication/AuthenticationService.java
+++ server/src/main/java/com/songspot/server/authentication/AuthenticationService.java
@@ -1,8 +1,18 @@
 package com.songspot.server.authentication;
 
+import com.songspot.server.configuration.JwtConfig;
+import com.songspot.server.controller.model.User;
+import com.songspot.server.controller.model.UserType;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
@@ -10,11 +20,57 @@ public class AuthenticationService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    private JwtConfig jwtConfig;
+
     public String encode(String password) {
         return bCryptPasswordEncoder.encode(password);
     }
 
     public boolean matches(String password, String expected) {
         return this.bCryptPasswordEncoder.matches(password, expected);
+    }
+
+    /**
+     * Tries to parse specified String as a JWT token. If successful, returns User object with username, id and role prefilled (extracted from token).
+     * If unsuccessful (token is invalid or not containing all required user properties), simply returns null.
+     *
+     * @param token the JWT token to parse
+     * @return the User object extracted from specified token or null if a token is invalid.
+     */
+    public User parseToken(String token) {
+        try {
+            Claims body = Jwts.parserBuilder().setSigningKey(jwtConfig.getSecret()).build().parseClaimsJws(token)
+                    .getBody();
+            User user = new User();
+            user.setUsername(body.getSubject());
+            user.setId(Long.parseLong((String) body.get("userId")));
+            user.setUserType(UserType.valueOf((String) body.get("userType")));
+
+            return user;
+
+        } catch (JwtException | ClassCastException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Generates a JWT token containing username as subject, and userId and role as additional claims. These properties are taken from the specified
+     * User object. Tokens validity is infinite.
+     *
+     * @param user the user for which the token will be generated
+     * @return the JWT token
+     */
+    public String generateToken(User user) {
+        Claims claims = Jwts.claims().setSubject(user.getUsername());
+        claims.put("userId", user.getId().toString());
+        claims.put("userType", user.getUserType());
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(jwtConfig.getSecret())
+                .signWith(key)
+                .compact();
     }
 }
