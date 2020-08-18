ca05545fa4bd9d65174e937e7c3eeccdba16d91b
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-17 23:30:30 -0700

    Implement token authentication in authentication filter

--- server/src/main/java/com/songspot/server/filter/AuthenticationFilter.java
+++ server/src/main/java/com/songspot/server/filter/AuthenticationFilter.java
@@ -1,8 +1,10 @@
 package com.songspot.server.filter;
 
+import com.songspot.server.authentication.AuthenticationService;
 import com.songspot.server.client.UserClient;
 import com.songspot.server.configuration.AuthenticationConfig;
 import com.songspot.server.controller.UserController;
+import com.songspot.server.controller.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -29,6 +31,9 @@ public class AuthenticationFilter implements Filter {
     @Autowired
     private AuthenticationConfig authenticationConfig;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
@@ -37,14 +42,25 @@ public class AuthenticationFilter implements Filter {
         Enumeration<String> headerNames = request.getHeaderNames();
 
         String username = null;
+        boolean tokenExpired = false;
         if (headerNames != null) {
             while (headerNames.hasMoreElements()) {
                 String name = headerNames.nextElement();
                 String val = request.getHeader(name);
-                if (UserClient.USER_NAME_KEY.equals(name)) {
+                if (UserClient.USER_TOKEN.equals(name)) {
+                    String token = URLDecoder.decode(val, StandardCharsets.UTF_8.toString());
+                    User user = authenticationService.parseToken(token);
+                    if(Objects.isNull(user)) {
+                        tokenExpired = true;
+                        break;
+                    }
 
+                    username = user.getUsername();
+                    MDC.put(UserClient.USER_NAME_KEY, username);
+                    break;
+                }
+                if(this.authenticationConfig.isEnableUserHeader() && UserClient.USER_NAME_KEY.equals(name)) {
                     username = URLDecoder.decode(val, StandardCharsets.UTF_8.toString());
-
                     MDC.put(UserClient.USER_NAME_KEY, username);
                     break;
                 }
@@ -70,6 +86,14 @@ public class AuthenticationFilter implements Filter {
             cookie.setMaxAge(0);
             response.addCookie(cookie);
             response.addHeader("Location", "https://www.songspot.cn/home/index.html");
+        }
+        if (tokenExpired) {
+            Cookie cookie = new Cookie("proxy", null);
+            cookie.setPath("/");
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+            response.addHeader("Location", "https://www.songspot.cn/login");
         }
         chain.doFilter(req, res);
     }
