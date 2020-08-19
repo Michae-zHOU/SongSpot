20a4ae6ca58977c354587a9263f7d0213752f7fb
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-18 22:28:22 -0700

    Remvoe unused MDC

--- server/src/main/java/com/songspot/server/client/UserClient.java
+++ server/src/main/java/com/songspot/server/client/UserClient.java
@@ -2,7 +2,5 @@ package com.songspot.server.client;
 
 public class UserClient {
     public static final String USER_NAME_KEY = "username";
-    public static final String USER_TOKEN = "authentication";
-    public static final String USER_TYPE = "usertype";
-    public static final String USER_ID = "userid";
+    public static final String USER_TOKEN_KEY = "authentication";
 }
